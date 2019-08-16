28033145b4c39db21af1bd38db4949b315dcf0ef
AbhiDhir <dhir.abhimanyu@gmail.com>
2019-08-16 10:03:20 -0500

    initial commit

---
+++ src/components/about.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Me</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a computer science and mathematics major at UT Austin. I love problem solving - and am interested in many areas of computer science, especially data science and cryptography. I am good at picking up new skills quickly and applying information to solve real-life problems. I am looking to be part of a dynamic team in the area of software development.</p>
+                    <p>Currently I am a part of ACM (Association for Computation and Machinery) where I participate in coding challenges and workshops. I am also a part of ISSS (Information and Systems Security Society) which participates in CTFs and explores the realm of cybersecurity. </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
